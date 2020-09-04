f5c9bae62cf0fc8971269d9fdbfe38d5f61678ed
iftekher-hasib <67557443+iftekher-hasib@users.noreply.github.com>
2020-09-04 19:54:38 +0600

    false true

---
+++ true-false.js
@@ -0,0 +1,16 @@
+const age = 4;
+// 0  is false
+const name = ""
+// "" is false, all other is true.
+// undefine is false
+// Nan, Null, false are false
+
+// "0", " ", {}, [], etc are True
+
+
+if (name.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
